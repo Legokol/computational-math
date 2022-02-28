125615d7ec48199af11e38a67d140866cb351291
legokol <legokol3@gmail.com>
2022-02-28 10:53:18 +0300

    Some actually working version, testing in progress.

--- 2_sem/practice 1/task1/task1.cpp
+++ 2_sem/practice 1/task1/task1.cpp
@@ -8,8 +8,8 @@
 
 struct Point {
     // Структура, описывающая точку сетки
-    double x;
     double t;
+    double x;
 };
 
 struct Solution {
@@ -26,25 +26,38 @@ private:
 public:
 
     std::vector<std::vector<Solution>> calc(double T, double L, double tau, double h) {
-        grid.resize(L / h + 1);
+        grid.resize(T / tau + 1);
         std::vector<std::vector<Solution>> solution(grid.size());
 
-        for (int i = 0; i < grid.size(); ++i) {
-            // Заполнение массива сетки и начальных условий
-            grid[i].resize(T / tau + 1);
-            for (int j = 0; j < grid[i].size(); ++j) {
-                grid[i][j].x = i * h;
-                grid[i][j].t = j * tau;
-            }
-            solution[i].resize(grid[i].size());
-            solution[i][0] = Solution{{grid[i][0].x, 0}, sin(4 * M_PI * grid[i][0].x / L)};
+        grid[0].resize(L / h + 1);
+        solution[0].resize(grid[0].size());
+        for (int j = 0; j < grid[0].size(); ++j) {
+            // Задание начальных условий
+            grid[0][j].t = 0;
+            grid[0][j].x = h * j;
+            solution[0][j] = Solution{grid[0][j], sin(4 * M_PI * grid[0][j].x / L)};
         }
+
+        for (int i = 0; i < grid.size() - 1; ++i) {
+            grid[i + 1].resize(grid[i].size());
+            solution[i + 1].resize(solution[i].size());
+            for (int j = 1; j < grid[i].size(); ++j) {
+                solution[i + 1][j] = Solution{grid[i + 1][j],
+                                              solution[i][j].f + tau / h * (solution[i][j].f - solution[i][j - 1].f)};
+            }
+            solution[i + 1][0] = Solution{grid[i + 1][0], solution[i + 1].back().f};
+        }
+
         return solution;
     }
-
-    //Point step() {}
 };
 
 int main() {
+    double T = 18;
+    double L = 20;
+    double tau = 1;
+    double h = 1;
+    LeftAngle leftAngleSolver;
+    auto res = leftAngleSolver.calc(T, L, tau, h);
     return 0;
 }
