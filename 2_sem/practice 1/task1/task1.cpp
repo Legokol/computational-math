536b1f89484f4d18381dfc87fe83914770ee48ea
legokol <legokol3@gmail.com>
2022-03-06 00:39:04 +0300

    Lax-Wendroff method implemented

--- 2_sem/practice 1/task1/task1.cpp
+++ 2_sem/practice 1/task1/task1.cpp
@@ -30,16 +30,49 @@ public:
         solution[0].resize(n_x);
         for (int j = 0; j < n_x; ++j) {
             // Начальные условия
-            solution[0][j] = Solution{{0, j * h}, sin(4 * M_PI * (j * h) / L)};
+            solution[0][j] = {{0, j * h}, sin(4 * M_PI * (j * h) / L)};
         }
 
         for (int i = 0; i < n_t - 1; ++i) {
             solution[i + 1].resize((n_x));
             for (int j = 1; j < n_x; ++j) {
-                solution[i + 1][j] = Solution{{(i + 1) * tau, j * h},
-                                              solution[i][j].f - tau / h * (solution[i][j].f - solution[i][j - 1].f)};
+                solution[i + 1][j] = {{(i + 1) * tau, j * h},
+                                      solution[i][j].f - tau / h * (solution[i][j].f - solution[i][j - 1].f)};
             }
-            solution[i + 1][0] = Solution{{(i + 1) * tau, 0}, solution[i + 1].back().f};
+            solution[i + 1][0] = {{(i + 1) * tau, 0}, solution[i + 1].back().f};
+        }
+        return solution;
+    }
+};
+
+class LaxWendroff {
+public:
+    std::vector<std::vector<Solution>> calc(double T, double L, double tau, double h) {
+        int n_t = T / tau + 1;
+        int n_x = L / h + 1;
+        std::vector<std::vector<Solution>> solution(n_t);
+
+        solution[0].resize(n_x);
+        for (int j = 0; j < n_x; ++j) {
+            // Начальные условия
+            solution[0][j] = Solution{{0, j * h}, sin(4 * M_PI * (j * h) / L)};
+        }
+
+        for (int i = 0; i < n_t - 1; ++i) {
+            solution[i + 1].resize((n_x));
+            for (int j = 1; j < n_x - 1; ++j) {
+                solution[i + 1][j] = {{(i + 1) * tau, j * h},
+                                      solution[i][j].f - tau / (2 * h) * (solution[i][j + 1].f - solution[i][j - 1].f -
+                                                                          tau / h *
+                                                                          (solution[i][j + 1].f - 2 * solution[i][j].f +
+                                                                           solution[i][j - 1].f))};
+            }
+            solution[i + 1].back() = {(i + 1) * tau, (n_x - 1) * h,
+                                      solution[i].back().f -
+                                      tau / (2 * h) * (solution[i][0].f - solution[i][n_x - 2].f -
+                                                       tau / h * (solution[i][0].f - 2 * solution[i][n_x - 1].f +
+                                                                  solution[i][n_x - 2].f))};
+            solution[i + 1][0] = {{(i + 1) * tau, 0}, solution[i + 1].back().f};
         }
         return solution;
     }
@@ -59,13 +92,19 @@ int main() {
     auto leftAngleSol2 = leftAngleSolver.calc(T, L, cfl2 * h, h);
     auto leftAngleSol3 = leftAngleSolver.calc(T, L, cfl3 * h, h);
 
+    LaxWendroff laxWendroff;
+
+    auto LaxWendroffSol1 = laxWendroff.calc(T, L, cfl1 * h, h);
+    auto LaxWendroffSol2 = laxWendroff.calc(T, L, cfl2 * h, h);
+    auto LaxWendroffSol3 = laxWendroff.calc(T, L, cfl3 * h, h);
+
     /*std::ofstream writer;
-    writer.open("LeftAngle1.01.txt");
-    for (int i = 0; i < leftAngleSol3.size(); ++i) {
-        for (int j = 0; j < leftAngleSol3[i].size() - 1; ++j) {
-            writer << leftAngleSol3[i][j].f << ',';
+    writer.open("LaxWendroff1.01.txt");
+    for (int i = 0; i < LaxWendroffSol3.size(); ++i) {
+        for (int j = 0; j < LaxWendroffSol3[i].size() - 1; ++j) {
+            writer << LaxWendroffSol3[i][j].f << ',';
         }
-        writer << leftAngleSol3[i].back().f << std::endl;
+        writer << LaxWendroffSol3[i].back().f << std::endl;
     }
     writer.close();*/
 
